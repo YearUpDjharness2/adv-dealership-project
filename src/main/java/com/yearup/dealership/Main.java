c84362ccff8516fa088b535f4b8ab20eff671394
Djharness <djharness2@gmail.com>
2024-10-30 16:33:03 -0400

    checkpoint

---
+++ src/main/java/com/yearup/dealership/Main.java
@@ -0,0 +1,11 @@
+package com.yearup.dealership;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        ui.display();
+    }
+}
+
